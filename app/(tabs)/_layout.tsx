1cdb6b056205e173ce65129880527a832f8f576b
hadiial <salmanhadialghifari3@gmail.com>
2025-12-06 21:41:17 +0700

    update the bottom nav bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,29 +4,18 @@ import React from 'react';
 const TabLayout = () => {
   return (
     <Tabs screenOptions={{ headerShown: false }}>
-      
-      {/* Home (index) diletakkan PALING ATAS agar menjadi default screen saat aplikasi dibuka.
-        Gunakan `href: null` untuk menyembunyikannya dari Tab Bar bawah.
-      */}
-      <Tabs.Screen 
-        name="index" 
-        options={{
-          href: null,
-        }} 
-      />
-
       {/* Tab-tab yang ditampilkan di bawah */}
       <Tabs.Screen 
         name="schedule" 
         options={{ title: 'Schedule' }} 
       />
       <Tabs.Screen 
-        name="chat" 
-        options={{ title: 'Chat' }} 
-      />
-      <Tabs.Screen 
         name="bookmark" 
         options={{ title: 'Bookmark' }} 
+      />
+      <Tabs.Screen 
+        name="index" 
+        options={{ title: 'Home' }} 
       />
       <Tabs.Screen 
         name="history" 
